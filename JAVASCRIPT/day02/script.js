27871655fe5718082840a775cd9c48be1aead61d
max125-c <pravintambe5671@gmail.com>
2025-01-14 23:02:32 +0530

    javascript

---
+++ JAVASCRIPT/day02/script.js
@@ -0,0 +1,47 @@
+/*console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")*/
+
+// defining the variable
+
+/* Boundries to define the variabe name
+1)  IN BETWEEN OF THE WE ABLE TO USE THE VARIABE AS A NAME
+"a-z,A-Z,0-9,&,_"
+2) DO NOT START WITH 
+0-9
+3)TWO VARIABLE CANNOT HAVE THE SAME NAME AS THE OTHER VARIABEE NAME 
+
+GOOD PROGRAMMING PRACTICES
+1)
+USE camel case  firsr letter is small and next new letters first letter will be capital
+*/
+
+//camel case variable defining
+
+var cityName = "Pune";
+var name = "mahesh";
+console.log(name);
+var name = "mango";
+
+console.log(cityName);
+console.log(name);
+
+var number;
+console.log(number);
+
+// Instead of using var we use let and const
+// because it is more efficient than the var 
+//const is constant it cannot be change 
+
+//const marks = 91;
+// let marks = 99;
+// console.log(marks)
+let marks = 30;
+console.log(marks);
+const marks1 = 10;
+// const marks1;
+console.log(marks1);
