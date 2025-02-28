3c5b30defb1d903ccde4811d40d008e34822dafa
max125-c <pravintambe5671@gmail.com>
2025-02-28 10:05:38 +0530

    added

---
+++ JAVASCRIPT/day24/script.js
@@ -0,0 +1,19 @@
+//this is not goes into infinite time 
+/*
+setTimeout(() => {
+    console.log("hello brother this is setTimeout ");
+},1000)
+//he runs infinite tieme according to time
+setInterval(() => {
+    console.log("hello brother this is setInterval ");
+},1110)
+*/
+
+console. log( " a");
+setTimeout(() => {
+        console. log( "b" )
+    },6000);
+
+    console. log("c" )
+    // ab
+    
