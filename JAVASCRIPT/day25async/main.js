ebc208383ba25e5412159ed9978f214e83c0c4dd
max125-c <pravintambe5671@gmail.com>
2025-03-03 09:44:31 +0530

    promise of javascript

---
+++ JAVASCRIPT/day25async/main.js
@@ -0,0 +1,7 @@
+const p1 = fetch("https://dummyjson.com/todos"
+)
+// there data and err are just a name we can write any thing 
+console.log("a")
+p1.then((res ) => res.json()).then(data => console.log(data))
+.catch((err) => console.log(err));
+console.log("a")
