17a454aa15af2937f3820c4942dbb2a11758ae09
max125-c <pravintambe5671@gmail.com>
2025-03-18 09:49:15 +0530

    react state

--- react/react_state/src/App.jsx
+++ react/react_state/src/App.jsx
@@ -2,19 +2,54 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+{/* <Counter/> */}
 function App() {
 
-  const [count, updatecount] = useState(10);
+  const [count, updateshow] = useState(false);
   // const [count, updatecount] = arr;
   function handleClick(){
-    // count = count +1;
-    updatecount(count + 1);
+    updateshow (!count)
   }
+
+
+  // const [count, updatecount] = useState(0);
+  // // const [count, updatecount] = arr;
+  // function increment(){
+
+  //   updatecount(count + 1);
+ 
+  // }
+  // function decrement(){
+ 
+  //   updatecount(count - 1);
+ 
+  // }
+  // function reset(){
+  
+  //   updatecount( 0);
+ 
+  // }
+
+
+  // const [count, updatecount] = useState("srushti");
+  // // const [count, updatecount] = arr;
+  // function handleClick(){
+
+  //   // updatecount("srushti is smart");
+  //   updatecount(count + 7);
+  //   updatecount(count + 1);
+  //   updatecount(count + 3);
+    
+  // }
   return (
     <>
       <h1>count is {count}</h1>
-      <button onClick={handleClick}>click me</button>
+      <button onClick={handleClick}>hide/show</button>
+      {count && <h1>Hello</h1>}
+      {/* <button onClick={handleClick}>increase me</button> */}
+      {/* <button onClick={decrement}>decrese me</button>
+      <button onClick={reset}>reset me</button> */}
+      
       </>
   )
 }
